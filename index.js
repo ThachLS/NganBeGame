895d343477d71b8b5fa522e63065790d1e39a5ef
ThachLS <laisonthach98@gmail.com>
2025-04-03 23:51:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1-6
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png"); // image 1
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png"); // image 2
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩 Ngân Bé!";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+  document.querySelector("h1").innerHTML = "🚩 Ngân Lớn!";
 }
 else {randomNumber1 === randomNumber2
-  document.querySelector("h1").innerHTML = "🏳 Draw!";
+  document.querySelector("h1").innerHTML = "🏳 Ngân Chưa Lớn Lắm!";
 }
