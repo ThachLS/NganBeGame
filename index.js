26d92826142600a90b8ea452b3253ff7c3c42426
ThachLS <laisonthach98@gmail.com>
2025-04-03 23:39:43 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1-6
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png"); // image 1
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png"); // image 2
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+}
+else {randomNumber1 === randomNumber2
+  document.querySelector("h1").innerHTML = "🏳 Draw!";
+}
